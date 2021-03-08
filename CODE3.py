69bcd069f95ba124ae039b9d9f34266870f20258
Shruti012002 <shrutiagarwalajgm@gmail.com>
2021-03-08 11:47:43 +0530

    added 2 more files to branch1

---
+++ CODE3.py
@@ -0,0 +1,10 @@
+#LOOP
+i=1
+while(i<=3):
+    print("Ajay:)",end=" ")
+    j=1
+    while(j<=2):
+        print(" Rocks",end=" ")
+        j=j+1
+    i+=1
+    print()
---
+++ CODE4_For loop.py
@@ -0,0 +1,29 @@
+#for loop
+"""age =19
+txt="Shruti is {} years old"
+print(txt.format(age))
+
+txt="shruti"
+print(txt.upper())
+
+txt=" kriti is old now!"
+print(txt.strip())
+
+text="Shruti is good in C and python"
+if "good" in text:
+    print("'good' is present")
+else:
+    print("absent!!")
+
+t="Hello ,World!"
+print(t.replace("H","J"))
+
+list=["apple","orange","banana"]
+#list[2:]=["mango"]
+list.insert(2,"blackcurrant")
+list.append("kiwi")
+print(list)"""
+
+for i in range(1,21):
+    if(i%5!=0):
+       print(i)
