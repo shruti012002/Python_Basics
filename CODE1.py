358d9d2f8032626bcabce229db2ef495b9ee9678
Shruti012002 <shrutiagarwalajgm@gmail.com>
2021-03-08 11:41:49 +0530

    added python files to the repo

---
+++ CODE1.py
@@ -0,0 +1,6 @@
+from math import sqrt,pow;
+x=sqrt(25)
+print(x)
+y=pow(4,2)
+print(y)
+
---
+++ CODE2.py
@@ -0,0 +1,11 @@
+#if-else-if
+x=int(input("NUMBER?"))
+if(x==1):
+    print("one")
+elif(x==2):
+    print("two")
+elif(x==3):
+    print("three")
+else:
+    print("Not found")
+#print("Bye")
